5468224342368b7d817bf07063cb6f8b3a793107
arnabjavaro <roy.arnab0594@gmail.com>
2024-10-19 20:46:55 +0530

    HashMap Own Implementation Commit

---
+++ src/HashMapImplementations/Driver.java
@@ -0,0 +1,27 @@
+package HashMapImplementations;
+
+
+public class Driver {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MyHashMap<String, String> myHashMap = new MyHashMap<String, String>();
+        myHashMap.put("A", "B");
+        myHashMap.put("E", "F");
+        myHashMap.put("H", "P");
+        myHashMap.put("P", "2");
+        myHashMap.put("1",  "G");
+        myHashMap.put("2", "6");
+        myHashMap.put("3", "2");
+        myHashMap.put("4", "4");
+        myHashMap.put("1",  "H");
+
+
+//    System.out.println(myHashMap);
+//    System.out.println(myHashMap.get("A"));
+//    System.out.println(myHashMap.remove("A"));
+        myHashMap.remove("2");
+        System.out.println(myHashMap);
+    }
+
+}
